33f2dabd2b89a19bda2e52d51df52c493a48815f
Nico Leidner <nico.leidner@stud.hs-ruhrwest.de>
2023-05-31 09:40:46 +0200

    Added more things

--- RobotKarl/src/main/java/de/fhg/iais/openroberta/visitor/IKarlVisitor.java
+++ RobotKarl/src/main/java/de/fhg/iais/openroberta/visitor/IKarlVisitor.java
@@ -1,6 +1,14 @@
 package de.fhg.iais.openroberta.visitor;
 
+import de.fhg.iais.roberta.syntax.action.spike.PlayNoteAction;
+import de.fhg.iais.roberta.syntax.action.spike.PlayToneAction;
+import de.fhg.iais.roberta.syntax.sensor.generic.KeysSensor;
 import de.fhg.iais.roberta.visitor.IVisitor;
 
 public interface IKarlVisitor<V> extends IVisitor<V> {
+
+    V visitKeysSensor(KeysSensor keysSensor);
+    V visitPlayToneAction(PlayToneAction playToneAction);
+    V visitPlayNoteAction(PlayNoteAction playNoteAction);
+
 }
--- RobotKarl/src/main/java/de/fhg/iais/openroberta/visitor/KarlPythonVisitor.java
+++ RobotKarl/src/main/java/de/fhg/iais/openroberta/visitor/KarlPythonVisitor.java
@@ -1,5 +1,6 @@
 package de.fhg.iais.openroberta.visitor;
 
+
 import java.util.List;
 
 import com.google.common.collect.ClassToInstanceMap;
@@ -8,13 +9,17 @@ import de.fhg.iais.roberta.bean.IProjectBean;
 import de.fhg.iais.roberta.bean.UsedHardwareBean;
 import de.fhg.iais.roberta.components.ConfigurationAst;
 import de.fhg.iais.roberta.syntax.Phrase;
+import de.fhg.iais.roberta.syntax.action.spike.PlayNoteAction;
+import de.fhg.iais.roberta.syntax.action.spike.PlayToneAction;
 import de.fhg.iais.roberta.syntax.configuration.ConfigurationComponent;
 import de.fhg.iais.roberta.syntax.lang.blocksequence.MainTask;
 import de.fhg.iais.roberta.syntax.lang.stmt.WaitStmt;
 import de.fhg.iais.roberta.syntax.lang.stmt.WaitTimeStmt;
+import de.fhg.iais.roberta.syntax.sensor.generic.KeysSensor;
 import de.fhg.iais.roberta.syntax.sensor.generic.TimerReset;
 import de.fhg.iais.roberta.syntax.sensor.generic.TimerSensor;
 import de.fhg.iais.roberta.util.basic.C;
+import de.fhg.iais.roberta.util.dbc.DbcException;
 import de.fhg.iais.roberta.util.syntax.SC;
 import de.fhg.iais.roberta.visitor.lang.codegen.prog.AbstractPythonVisitor;
 
@@ -29,9 +34,34 @@ public class KarlPythonVisitor extends AbstractPythonVisitor implements IKarlVis
      * @param beans
      */
     protected KarlPythonVisitor(
-        List<List<Phrase>> programPhrases, ClassToInstanceMap<IProjectBean> beans, ConfigurationAst configurationAst) {
+        List<List<Phrase>> programPhrases,
+        ClassToInstanceMap<IProjectBean> beans,
+        ConfigurationAst configurationAst) {
         super(programPhrases, beans);
         this.configurationAst = configurationAst;
+    }
+
+    @Override
+    public Void visitKeysSensor(KeysSensor keysSensor) {
+        String portName = keysSensor.getUserDefinedPort();
+        ConfigurationComponent configurationComponent = this.configurationAst.getConfigurationComponent(portName);
+        String port = configurationComponent.getProperty("PIN1");
+        if ( port.equals("Knopf")) {
+            this.src.add("hub.right_button.is_pressed()");
+        }else{
+            throw new DbcException("Invalid key sensor port: " + port);
+        }
+        return null;
+    }
+
+    @Override
+    public Void visitPlayToneAction(PlayToneAction playToneAction) {
+        return null;
+    }
+
+    @Override
+    public Void visitPlayNoteAction(PlayNoteAction playNoteAction) {
+        return null;
     }
 
     @Override
@@ -46,16 +76,24 @@ public class KarlPythonVisitor extends AbstractPythonVisitor implements IKarlVis
 
     @Override
     public Void visitTimerReset(TimerReset timerReset) {
+        this.src.add("timer.reset()");
         return null;
     }
 
     @Override
     public Void visitWaitStmt(WaitStmt waitStmt) {
+        this.sb.append("while True:");
+        incrIndentation();
+        visitStmtList(waitStmt.statements);
+        decrIndentation();
         return null;
     }
 
     @Override
     public Void visitWaitTimeStmt(WaitTimeStmt waitTimeStmt) {
+        this.sb.append("wait_for_seconds(");
+        waitTimeStmt.time.accept(this);
+        this.sb.append("/1000)");
         return null;
     }
 
@@ -70,67 +108,54 @@ public class KarlPythonVisitor extends AbstractPythonVisitor implements IKarlVis
         if ( usedHardwareBean.isActorUsed(C.RANDOM) || usedHardwareBean.isActorUsed(C.RANDOM_DOUBLE) ) {
             this.src.add("import random").nlI();
         }
-        //this.src.add("from spike.control import wait_for_seconds, wait_until");
-        if ( usedHardwareBean.isSensorUsed(SC.TIMER) ) {
-            this.src.add(", Timer");
-        }
-        if ( !usedHardwareBean.getUsedActors().isEmpty() && !usedHardwareBean.getUsedSensors().isEmpty() ) {
-            nlIndent();
-        }
-        if ( usedHardwareBean.isActorUsed(SC.DIFFERENTIALDRIVE) ) {
-            ConfigurationComponent diffDrive = this.configurationAst.optConfigurationComponentByType("DIFFERENTIALDRIVE");
-            String leftPort = diffDrive.getComponentProperties().get("MOTOR_L");
-            String rightPort = diffDrive.getComponentProperties().get("MOTOR_R");
-            nlIndent();
-            this.src.add("TRACKWIDTH = ").add(diffDrive.getComponentProperties().get("BRICK_TRACK_WIDTH")).nlI();
-            this.src.add("diff_drive = spike.MotorPair('").add(leftPort).add("', '").add(rightPort).add("')").nlI();
-            this.src.add("diff_drive.set_motor_rotation(").add(diffDrive.getComponentProperties().get("BRICK_WHEEL_DIAMETER")).add(" * math.pi, 'cm')");
-        }
-        if ( usedHardwareBean.isActorUsed(SC.MOTOR) ) {
-            usedHardwareBean.getUsedActors().stream().filter(usedActor -> usedActor.getType().equals("MOTOR")).forEach(motor -> {
-                nlIndent();
-                this.src.add("motor").add(motor.getPort()).add(" = spike.Motor('").add(motor.getPort()).add("')");
-            });
-        }
-        if ( usedHardwareBean.isSensorUsed(SC.TOUCH) ) {
+
+        if(usedHardwareBean.isSensorUsed(SC.KEY)){
             usedHardwareBean.getUsedSensors().stream().filter(usedActor -> usedActor.getType().equals("TOUCH")).forEach(sensor -> {
                 if ( configurationAst.optConfigurationComponent(sensor.getPort()) != null ) {
                     nlIndent();
-                    this.src.add("touch_sensor_").add(sensor.getPort()).add(" = spike.ForceSensor('").add(getPortFromConfig(sensor.getPort())).add("')");
+                    this.src.add("button").add(sensor.getPort()).add(" = Button('").add(getPortFromConfig(sensor.getPort())).add("')");
                 }
             });
         }
-        if ( usedHardwareBean.isSensorUsed(SC.ULTRASONIC) ) {
-            usedHardwareBean.getUsedSensors().stream().filter(usedActor -> usedActor.getType().equals("ULTRASONIC")).forEach(sensor -> {
+
+        if(usedHardwareBean.isSensorUsed(SC.POTENTIOMETER)){
+            usedHardwareBean.getUsedSensors().stream().filter(usedActor -> usedActor.getType().equals("TOUCH")).forEach(sensor -> {
                 if ( configurationAst.optConfigurationComponent(sensor.getPort()) != null ) {
                     nlIndent();
-                    this.src.add("ultrasonic_sensor_").add(sensor.getPort()).add(" = spike.DistanceSensor('").add(getPortFromConfig(sensor.getPort())).add("')");
+                    this.src.add("poti").add(sensor.getPort()).add(" = Rotary('").add(getPortFromConfig(sensor.getPort())).add("')");
                 }
             });
         }
-        if ( usedHardwareBean.isSensorUsed(SC.COLOR) ) {
-            usedHardwareBean.getUsedSensors().stream().filter(usedActor -> usedActor.getType().equals("COLOR")).forEach(sensor -> {
-                if ( configurationAst.optConfigurationComponent(sensor.getPort()) != null ) {
-                    nlIndent();
-                    this.src.add("color_sensor_").add(sensor.getPort()).add(" = spike.ColorSensor('").add(getPortFromConfig(sensor.getPort())).add("')");
-                }
+
+        //TODO Motoren unterscheiden in Beine/Füße
+        if ( usedHardwareBean.isActorUsed(SC.MOTOR) ) {
+            usedHardwareBean.getUsedActors().stream().filter(usedActor -> usedActor.getType().equals("MOTOR")).forEach(motor -> {
+                nlIndent();
+                this.src.add("motor").add(motor.getPort()).add(" = Servo('").add(motor.getPort()).add("')");
             });
         }
-        if ( usedHardwareBean.isActorUsed("DISPLAY") ) {
-            nlIndent();
-            this.src.add("import hub as _hub").nlI();
-            this.src.add("display = _hub.display").nlI();
-            this.src.add("Image = _hub.Image");
+
+        if ( usedHardwareBean.isActorUsed(SC.RGBLED) ) {
+            usedHardwareBean.getUsedActors().stream().filter(usedActor -> usedActor.getType().equals("RGBLED")).forEach(motor -> {
+                nlIndent();
+                this.src.add("eye").add(motor.getPort()).add(" = LED('").add(motor.getPort()).add("')");
+            });
         }
-        if ( usedHardwareBean.isSensorUsed(SC.TIMER) ) {
-            nlIndent();
-            this.src.add("timer = Timer()");
+
+        if ( usedHardwareBean.isActorUsed(SC.BUZZER) ) {
+            usedHardwareBean.getUsedActors().stream().filter(usedActor -> usedActor.getType().equals("BUZZER")).forEach(motor -> {
+                nlIndent();
+                this.src.add("mouth").add(motor.getPort()).add(" = Speaker('").add(motor.getPort()).add("')");
+            });
         }
-        if ( usedHardwareBean.isActorUsed("HUB") ) {
+
+        if ( !usedHardwareBean.getUsedActors().isEmpty() && !usedHardwareBean.getUsedSensors().isEmpty() ) {
             nlIndent();
-            this.src.add("hub = spike.PrimeHub()");
         }
+
+
     }
+
 
     private String getPortFromConfig(String name) {
         ConfigurationComponent block = configurationAst.getConfigurationComponent(name);
