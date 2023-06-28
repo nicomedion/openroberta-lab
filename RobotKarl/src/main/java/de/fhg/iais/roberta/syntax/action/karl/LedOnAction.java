100de8c07f5afc94667a24593402385b8822f40d
Nico Leidner <nico.leidner@stud.hs-ruhrwest.de>
2023-06-28 12:06:32 +0200

    Toolbox for karl is now shown correctly

---
+++ RobotKarl/src/main/java/de/fhg/iais/roberta/syntax/action/karl/LedOnAction.java
@@ -0,0 +1,7 @@
+package de.fhg.iais.roberta.syntax.action.karl;
+
+import de.fhg.iais.roberta.transformer.forClass.NepoPhrase;
+
+//@NepoPhrase(category = "ACTOR", blocklyNames = {"actions_brickled_on"}, name = "LED_ON_ACTION")
+public class LedOnAction {
+}
