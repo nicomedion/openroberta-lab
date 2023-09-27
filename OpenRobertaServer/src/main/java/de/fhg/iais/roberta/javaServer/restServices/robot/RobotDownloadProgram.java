f230e837f9a8808bdd7ac39dfeecaf932529d32b
Nico Leidner <nico.leidner@medion.com>
2023-09-27 15:57:00 +0200

    Added karl to RobotDownloadProgram

--- OpenRobertaServer/src/main/java/de/fhg/iais/roberta/javaServer/restServices/robot/RobotDownloadProgram.java
+++ OpenRobertaServer/src/main/java/de/fhg/iais/roberta/javaServer/restServices/robot/RobotDownloadProgram.java
@@ -66,6 +66,7 @@ public class RobotDownloadProgram {
                 case "ev3dev":
                 case "mbot2":
                 case "spike":
+                case "karl":
                     fileName = programName + ".py";
                     filePath = this.pathToCrosscompilerBaseDir + token + "/" + programName + "/source";
                     break;
